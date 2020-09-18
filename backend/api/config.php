e319067ee0198ebfe6f35628293bc113c65699d2
Kelvin Morrison <kelvinninja1@gmail.com>
2020-09-18 09:23:50 +0000

    Adde: Config Details

---
+++ backend/api/config.php
@@ -0,0 +1,21 @@
+<?php
+
+// $host;
+// $dbName;
+// $username;
+// $password;
+
+
+// for production
+// $host = "localhost";
+// $dbName = "ourcomunmmy_invoicebackend_db";
+// $username = "ourcomunmmy_caeser";
+// $password = "Pa55word41#";
+
+// for development
+$host = "localhost";
+$dbName = "invoicebackend_db";
+$username = "root";
+$password = "";
+
+ ?>
