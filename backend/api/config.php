be27af9a315324d99ebcceaa7439a971627e0818
Kelvin Morrison <kelvinninja1@gmail.com>
2020-09-19 13:33:01 +0000

    Added: Online Credentials

--- backend/api/config.php
+++ backend/api/config.php
@@ -7,15 +7,15 @@
 
 
 // for production
-// $host = "localhost";
-// $dbName = "ourcomunmmy_invoicebackend_db";
-// $username = "ourcomunmmy_caeser";
-// $password = "Pa55word41#";
+$host = "localhost";
+$dbName = "ourcomunmmy_invoicebackend_db";
+$username = "ourcomunmmy_caeser";
+$password = "Pa55word41#";
 
 // for development
-$host = "localhost";
-$dbName = "invoicebackend_db";
-$username = "root";
-$password = "";
+// $host = "localhost";
+// $dbName = "invoicebackend_db";
+// $username = "root";
+// $password = "";
 
  ?>
